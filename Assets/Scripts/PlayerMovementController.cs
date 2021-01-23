886cedffcb1f01cb6046fbd2cc4910de61e716b7
pgpais <pgpaisdev@gmail.com>
2021-01-23 14:10:07 +0000

    added groundCheck and spriteFlip

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovementController : MonoBehaviour
 {
+    [SerializeField] float speed = 5f;
     [SerializeField] float jumpMaxTime = 0.35f;
     [SerializeField] float jumpForce = 10f;
-    [SerializeField] float speed = 5f;
+
+    [Header("Ground Check")]
+    [SerializeField] Transform groundCheckPoint;
+    [SerializeField] float groundCheckRadius = 0.5f;
+    [SerializeField] LayerMask groundLayer;
 
     Rigidbody2D rb;
     bool isGrounded = true;
@@ -29,17 +34,28 @@ public class PlayerMovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckGround();
         HandleJumping();
         HandleMovement();
     }
 
+    void CheckGround(){
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+    }
+
     void HandleMovement(){
+        if(movInput.x > 0){
+            // Look right
+            transform.localScale = new Vector3(1, 1, 1);
+        } else{
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
         rb.velocity = new Vector2(movInput.x * speed, rb.velocity.y);
     }
     
     void HandleJumping(){
         if(isGrounded && isJumping){
-            jumpTimeLimit = Time.time + jumpTimeLimit;
+            jumpTimeLimit = Time.time + jumpMaxTime;
             rb.velocity = Vector2.up * jumpForce;
         } else if(isJumping){
             if(Time.time < jumpTimeLimit){
